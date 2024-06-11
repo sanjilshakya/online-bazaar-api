bdf92f58367ebff179f53ad0f94923c2ef18ff12
Sanjil Shakya <sanjil@noveltytechnology.com>
2024-06-11 10:37:52 -0400

    global error handler added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const userRouter = require("./routes/userRouter");
 const categoryRouter = require("./routes/categoryRouter");
 const productRouter = require("./routes/productRouter");
 const orderRouter = require("./routes/orderRouter");
+const { globalErrorHandler } = require("./controllers/errorController");
 
 const app = express();
 
@@ -13,5 +14,7 @@ app.use("/api/users", userRouter);
 app.use("/api/categories", categoryRouter);
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
+
+app.use(globalErrorHandler);
 
 module.exports = app;
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,55 +1,45 @@
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
 
-exports.signUp = async (req, res, next) => {
-  try {
-    const { firstName, lastName, email, password, confirmPassword } = req.body;
-    const user = await User.create({
-      firstName,
-      lastName,
-      email,
-      password,
-      confirmPassword,
-    });
+exports.signUp = catchAsync(async (req, res, next) => {
+  const { firstName, lastName, email, password, confirmPassword } = req.body;
+  const user = await User.create({
+    firstName,
+    lastName,
+    email,
+    password,
+    confirmPassword,
+  });
 
-    res.status(201).json({
-      status: "success",
-      data: {
-        user,
-      },
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: "fail",
-      error,
-    });
-  }
-};
+  res.status(201).json({
+    status: "success",
+    data: {
+      user,
+    },
+  });
+});
 
-exports.login = async (req, res, next) => {
-  try {
-    if (!req.body.email || !req.body.password)
-      throw new Error("email and password are required field");
-    const user = await User.findOne({ email: req.body.email }).select('+password');
+exports.login = catchAsync(async (req, res, next) => {
+  if (!req.body.email || !req.body.password)
+    return next(new AppError("email and password are required field", 400));
+  const user = await User.findOne({ email: req.body.email }).select(
+    "+password"
+  );
 
-    if (!user || !(await user.verifyPassword(req.body.password, user.password)))
-      throw new Error("Incorrect email or password");
+  if (!user || !(await user.verifyPassword(req.body.password, user.password)))
+    return next(new AppError("Incorrect email or password", 400));
 
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET_KEY, {
-      expiresIn: process.env.JWT_EXPIRES_IN,
-    });
+  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
-    res.status(200).json({
-      status: "success",
-      token,
-      data: {
-        user,
-      },
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: "fail",
-      error,
-    });
-  }
-};
+  res.status(200).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+});
